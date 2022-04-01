38e18ca36242ec7a80fec01666d517874819d81a
“suchapalaver” <jlivesey@gmail.com>
2022-04-01 11:59:10 -0400

    Change `run` to take application address as argument

--- src/lib.rs
+++ src/lib.rs
@@ -1,16 +1,17 @@
 //! src/lib.rs
 use actix_web::dev::Server;
 use actix_web::{web, App, HttpResponse, HttpServer};
+
 async fn health_check() -> HttpResponse {
     HttpResponse::Ok().finish()
 }
+
 // Notice the different signature!
 // We return `Server` on the happy path and we dropped the `async` keyword
 // We have no .await call, so it is not needed anymore.
-pub fn run() -> Result<Server, std::io::Error> {
+pub fn run(address: &str) -> Result<Server, std::io::Error> {
     let server = HttpServer::new(|| App::new().route("/health_check", web::get().to(health_check)))
-        .bind("127.0.0.1:8000")?
+        .bind(address)?
         .run();
-    // No .await here!
     Ok(server)
 }
