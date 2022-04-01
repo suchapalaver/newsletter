e3b79c93d7fc15b8ca4dd3aa5eccfc8de9a7d675
“suchapalaver” <jlivesey@gmail.com>
2022-04-01 12:01:05 -0400

    Change all `newsletter::run()` to `run("127.0.0.1:8000")`

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,9 @@
 //! src/main.rs
 use newsletter::run;
+
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
     // Bubble up the io::Error if we failed to bind the address
     // Otherwise call .await on our Server
-    run()?.await
+    run("127.0.0.1:8000")?.await
 }
