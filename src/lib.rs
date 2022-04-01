63a6c34c0c8d1c792ca620be4c71d5bcd865d2a4
“suchapalaver” <jlivesey@gmail.com>
2022-04-01 10:51:17 -0400

    Refactor `newsletter::run` to return a `Server` without awaiting it

--- src/lib.rs
+++ src/lib.rs
@@ -1,17 +1,16 @@
-//! lib.rs
+//! src/lib.rs
+use actix_web::dev::Server;
 use actix_web::{web, App, HttpResponse, HttpServer};
 async fn health_check() -> HttpResponse {
     HttpResponse::Ok().finish()
 }
-// We need to mark `run` as public.
-// It is no longer a binary entrypoint, therefore we can mark it as async
-// without having to use any proc-macro incantation.
-pub async fn run() -> std::io::Result<()> {
-    HttpServer::new(|| {
-        App::new()
-            .route("/health_check", web::get().to(health_check))
-    })
+// Notice the different signature!
+// We return `Server` on the happy path and we dropped the `async` keyword
+// We have no .await call, so it is not needed anymore.
+pub fn run() -> Result<Server, std::io::Error> {
+    let server = HttpServer::new(|| App::new().route("/health_check", web::get().to(health_check)))
         .bind("127.0.0.1:8000")?
-        .run()
-        .await
+        .run();
+    // No .await here!
+    Ok(server)
 }
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
-//! main.rs
-use zero2prod::run;
+//! src/main.rs
+use newsletter::run;
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
-    run().await
+    // Bubble up the io::Error if we failed to bind the address
+    // Otherwise call .await on our Server
+    run()?.await
 }
