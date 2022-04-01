a04833643ae520b6695e570649783e83dfb0fd21
“suchapalaver” <jlivesey@gmail.com>
2022-04-01 13:50:38 -0400

    Use `actix-web` `Form` extractor as argument for /subscriptions handler

--- src/lib.rs
+++ src/lib.rs
@@ -10,10 +10,16 @@ use std::net::TcpListener;
 async fn health_check() -> HttpResponse {
     HttpResponse::Ok().finish()
 }
-// Let's start simple: we always return a 200 OK
-async fn subscribe() -> HttpResponse {
+
+#[derive(serde::Deserialize)]
+struct FormData {
+    email: String,
+    name: String
+}
+async fn subscribe(_form: web::Form<FormData>) -> HttpResponse {
     HttpResponse::Ok().finish()
 }
+
 pub fn run(listener: TcpListener) -> Result<Server, std::io::Error> {
     let server = HttpServer::new(|| {
         App::new()
