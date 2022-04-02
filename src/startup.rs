16ac405bed755b34ebc7d8c1b10dce75e84778f4
“suchapalaver” <jlivesey@gmail.com>
2022-04-02 16:11:48 -0400

    Use `web::Data` to wrap connection

--- src/startup.rs
+++ src/startup.rs
@@ -1,25 +1,23 @@
 //! src/startup.rs
 
+use crate::routes::{health_check, subscribe};
 use actix_web::dev::Server;
 use actix_web::{web, App, HttpServer};
 use sqlx::PgConnection;
 use std::net::TcpListener;
 
-use crate::routes::{health_check, subscribe};
-
-pub fn run(
-    listener: TcpListener,
-// New parameter!
-    connection: PgConnection
-) -> Result<Server, std::io::Error> {
-    let server = HttpServer::new(|| {
+pub fn run(listener: TcpListener, connection: PgConnection) -> Result<Server, std::io::Error> {
+    // Wrap the connection in a smart pointer
+    let connection = web::Data::new(connection);
+    // Capture `connection` from the surrounding environment
+    let server = HttpServer::new(move || {
         App::new()
             .route("/health_check", web::get().to(health_check))
             .route("/subscriptions", web::post().to(subscribe))
-// Register the connection as part of the application state
-            .app_data(connection)
+            // Get a pointer copy and attach it to the application state
+            .app_data(connection.clone())
     })
-        .listen(listener)?
-        .run();
+    .listen(listener)?
+    .run();
     Ok(server)
 }
