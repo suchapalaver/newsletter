bbed5412227029e0f6ccdb972b489794071d18eb
“suchapalaver” <jlivesey@gmail.com>
2022-04-02 16:15:04 -0400

    Fix configuration in `main`

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,18 @@
 //! src/main.rs
 
-use std::net::TcpListener;
-
 use newsletter::configuration::get_configuration;
 use newsletter::startup::run;
 
+use sqlx::{Connection, PgConnection};
+use std::net::TcpListener;
+
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
-    // Panic if we can't read configuration
     let configuration = get_configuration().expect("Failed to read configuration.");
-    // We have removed the hard-coded `8000` - it's now coming from our settings!
+    let connection = PgConnection::connect(&configuration.database.connection_string())
+        .await
+        .expect("Failed to connect to Postgres.");
     let address = format!("127.0.0.1:{}", configuration.application_port);
     let listener = TcpListener::bind(address)?;
-    run(listener)?.await
+    run(listener, connection)?.await
 }
