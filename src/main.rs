daa94fee6c329775082eadaf26c6d37b8cc6146a
“suchapalaver” <jlivesey@gmail.com>
2022-04-02 13:35:17 -0400

    Modify `main` to read `configuration` as first step

--- src/main.rs
+++ src/main.rs
@@ -2,12 +2,15 @@
 
 use std::net::TcpListener;
 
+use newsletter::configuration::get_configuration;
 use newsletter::startup::run;
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
-    // Bubble up the io::Error if we failed to bind the address
-    // Otherwise call .await on our Server
-    let listener = TcpListener::bind("127.0.0.1:0").expect("Failed to bind random port");
+    // Panic if we can't read configuration
+    let configuration = get_configuration().expect("Failed to read configuration.");
+    // We have removed the hard-coded `8000` - it's now coming from our settings!
+    let address = format!("127.0.0.1:{}", configuration.application_port);
+    let listener = TcpListener::bind(address)?;
     run(listener)?.await
 }
