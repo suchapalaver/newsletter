49cc03fc10342591234358d44077ee3d2ecd4c74
“suchapalaver” <jlivesey@gmail.com>
2022-04-02 16:33:25 -0400

    Bind dynamic data to INSERT query in `subscribe` using `query!`

--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -1,7 +1,8 @@
 //! src/routes/subscriptions.rs
-
 use actix_web::{web, HttpResponse};
+use chrono::Utc;
 use sqlx::PgConnection;
+use uuid::Uuid;
 
 #[allow(dead_code)]
 #[derive(serde::Deserialize)]
@@ -11,9 +12,22 @@ pub struct FormData {
 }
 
 pub async fn subscribe(
-    _form: web::Form<FormData>,
-    // Retrieving a connection from the application state!
-    _connection: web::Data<PgConnection>,
+    form: web::Form<FormData>,
+    connection: web::Data<PgConnection>,
 ) -> HttpResponse {
+    sqlx::query!(
+        r#"
+INSERT INTO subscriptions (id, email, name, subscribed_at)
+VALUES ($1, $2, $3, $4)
+"#,
+        Uuid::new_v4(),
+        form.email,
+        form.name,
+        Utc::now()
+    )
+    // We use `get_ref` to get an immutable reference to the `PgConnection`
+    // wrapped by `web::Data`.
+    .execute(connection.get_ref())
+    .await;
     HttpResponse::Ok().finish()
 }
