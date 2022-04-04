5c66d03d5a4d4578a42035035ce701370b39dab9
“suchapalaver” <jlivesey@gmail.com>
2022-04-04 19:11:46 -0400

    Migrate from `log` to `tracing`

--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -18,13 +18,13 @@ pub async fn subscribe(
 ) -> HttpResponse {
     // Let's generate a random unique identifier
     let request_id = Uuid::new_v4();
-    log::info!(
+    tracing::info!(
         "request_id {} - Adding '{}' '{}' as a new subscriber.",
         request_id,
         form.email,
         form.name
     );
-    log::info!(
+    tracing::info!(
         "request_id {} - Saving new subscriber details in the database",
         request_id
     );
@@ -42,14 +42,14 @@ pub async fn subscribe(
     .await
     {
         Ok(_) => {
-            log::info!(
+            tracing::info!(
                 "request_id {} - New subscriber details have been saved",
                 request_id
             );
             HttpResponse::Ok().finish()
         }
         Err(e) => {
-            log::error!(
+            tracing::error!(
                 "request_id {} - Failed to execute query: {:?}",
                 request_id,
                 e
