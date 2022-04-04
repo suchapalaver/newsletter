0590c1391864439d3397064e340630e96aa06837
“suchapalaver” <jlivesey@gmail.com>
2022-04-04 18:06:46 -0400

    Add log records before query execution starts and after its completion

--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -16,6 +16,7 @@ pub async fn subscribe(
     form: web::Form<FormData>,
     pool: web::Data<PgPool>, // Renamed!
 ) -> HttpResponse {
+    log::info!("Saving new subscriber details in the database");
     match sqlx::query!(
         r#"
         INSERT INTO subscriptions (id, email, name, subscribed_at)
@@ -29,9 +30,12 @@ pub async fn subscribe(
     .execute(pool.as_ref())
     .await
     {
-        Ok(_) => HttpResponse::Ok().finish(),
+        Ok(_) => {
+            log::info!("New subscriber details have been saved");
+            HttpResponse::Ok().finish()
+        },
         Err(e) => {
-            println!("Failed to execute query: {}", e);
+            log::error!("Failed to execute query: {:?}", e);
             HttpResponse::InternalServerError().finish()
         }
     }
