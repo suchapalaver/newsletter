e384ba8ffbb965fa530f18145cf12c4185928532
“suchapalaver” <jlivesey@gmail.com>
2022-04-11 17:01:48 -0400

    Use `LogTracer` to explicitly register a logger implementation to redirect logs to our `tracing` subscriber for processing

--- src/main.rs
+++ src/main.rs
@@ -8,28 +8,23 @@ use sqlx::PgPool;
 use std::net::TcpListener;
 use tracing::subscriber::set_global_default;
 use tracing_bunyan_formatter::{BunyanFormattingLayer, JsonStorageLayer};
+use tracing_log::LogTracer;
 use tracing_subscriber::{layer::SubscriberExt, EnvFilter, Registry};
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
-    // Removed the `env_logger` line we had before!
+    // Redirect all `log`'s events to our subscriber
+    LogTracer::init().expect("Failed to set logger");
 
-    // Falling back to printing all spans at info-level or above
-    // if the RUST_LOG environment variable has not been set.
     let env_filter = EnvFilter::try_from_default_env().unwrap_or_else(|_| EnvFilter::new("info"));
-    let formatting_layer = BunyanFormattingLayer::new(
-        "newsletter".into(),
-        // Output the formatted spans to stdout.
-        std::io::stdout,
-    );
-    // The `with` method is provided by `SubscriberExt`, an extension
-    // trait for `Subscriber` exposed by `tracing_subscriber`
+
+    let formatting_layer = BunyanFormattingLayer::new("newsletter".into(), std::io::stdout);
+
     let subscriber = Registry::default()
         .with(env_filter)
         .with(JsonStorageLayer)
         .with(formatting_layer);
-    // `set_global_default` can be used by applications to specify
-    // what subscriber should be used to process spans.
+
     set_global_default(subscriber).expect("Failed to set subscriber");
 
     let configuration = get_configuration().expect("Failed to read configuration.");
@@ -37,7 +32,10 @@ async fn main() -> std::io::Result<()> {
     let connection_pool = PgPool::connect(&configuration.database.connection_string())
         .await
         .expect("Failed to connect to Postgres.");
+
     let address = format!("127.0.0.1:{}", configuration.application_port);
+
     let listener = TcpListener::bind(address)?;
+
     run(listener, connection_pool)?.await
 }
