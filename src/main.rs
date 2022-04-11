291f503a5fe83f006adbb49768ca06927d8254f3
“suchapalaver” <jlivesey@gmail.com>
2022-04-11 17:29:20 -0400

    Break down `main` with `get_subscriber` and `init_subscriber`

--- src/main.rs
+++ src/main.rs
@@ -1,41 +1,51 @@
 //! src/main.rs
-
 use newsletter::configuration::get_configuration;
 use newsletter::startup::run;
-
-// Removed `use env_logger::Env`
-use sqlx::PgPool;
+use sqlx::postgres::PgPool;
 use std::net::TcpListener;
-use tracing::subscriber::set_global_default;
+use tracing::{Subscriber, subscriber::set_global_default};
 use tracing_bunyan_formatter::{BunyanFormattingLayer, JsonStorageLayer};
 use tracing_log::LogTracer;
 use tracing_subscriber::{layer::SubscriberExt, EnvFilter, Registry};
 
-#[tokio::main]
-async fn main() -> std::io::Result<()> {
-    // Redirect all `log`'s events to our subscriber
-    LogTracer::init().expect("Failed to set logger");
-
-    let env_filter = EnvFilter::try_from_default_env().unwrap_or_else(|_| EnvFilter::new("info"));
-
-    let formatting_layer = BunyanFormattingLayer::new("newsletter".into(), std::io::stdout);
-
-    let subscriber = Registry::default()
+/// Compose multiple layers into a `tracing`'s subscriber.
+///
+/// # Implementation Notes
+///
+/// We are using `impl Subscriber` as return type to avoid having to
+/// spell out the actual type of the returned subscriber, which is
+/// indeed quite complex.
+/// We need to explicitly call out that the returned subscriber is
+/// `Send` and `Sync` to make it possible to pass it to `init_subscriber`
+/// later on.
+fn get_subscriber(name: String, env_filter: String) -> impl Subscriber + Send + Sync {
+    let env_filter =
+        EnvFilter::try_from_default_env().unwrap_or_else(|_| EnvFilter::new(env_filter));
+    let formatting_layer = BunyanFormattingLayer::new(name, std::io::stdout);
+    Registry::default()
         .with(env_filter)
         .with(JsonStorageLayer)
-        .with(formatting_layer);
-
+        .with(formatting_layer)
+}
+/// Register a subscriber as global default to process span data.
+///
+/// It should only be called once!
+pub fn init_subscriber(subscriber: impl Subscriber + Send + Sync) {
+    LogTracer::init().expect("Failed to set logger");
     set_global_default(subscriber).expect("Failed to set subscriber");
+}
+
+#[tokio::main]
+async fn main() -> std::io::Result<()> {
+    let subscriber = get_subscriber("newsletter".into(), "info".into());
+    init_subscriber(subscriber);
 
     let configuration = get_configuration().expect("Failed to read configuration.");
-
     let connection_pool = PgPool::connect(&configuration.database.connection_string())
         .await
         .expect("Failed to connect to Postgres.");
-
     let address = format!("127.0.0.1:{}", configuration.application_port);
-
     let listener = TcpListener::bind(address)?;
-
-    run(listener, connection_pool)?.await
+    run(listener, connection_pool)?.await?;
+    Ok(())
 }
