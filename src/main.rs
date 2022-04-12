85010b781ce973ba722fd9b983f87a81341a0bb9
“suchapalaver” <jlivesey@gmail.com>
2022-04-12 11:49:00 -0400

    Adjust `main` to use `stdout`

--- src/main.rs
+++ src/main.rs
@@ -7,7 +7,7 @@ use std::net::TcpListener;
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
-    let subscriber = get_subscriber("newsletter".into(), "info".into());
+    let subscriber = get_subscriber("newsletter".into(), "info".into(), std::io::stdout);
     init_subscriber(subscriber);
 
     let configuration = get_configuration().expect("Failed to read configuration.");
