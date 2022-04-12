8567a318ef87534997a2ff210f7fb3623e33ca68
“suchapalaver” <jlivesey@gmail.com>
2022-04-12 14:06:11 -0400

    Extract query in own function and use `tracing::instrument` to get rid of `query_span` and the call to the `.instrument` method

--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -1,9 +1,7 @@
 //! src/routes/subscriptions.rs
 use actix_web::{web, HttpResponse};
 use chrono::Utc;
-// No longer importing PgConnection!
 use sqlx::PgPool;
-use tracing::Instrument;
 use uuid::Uuid;
 
 #[allow(dead_code)]
@@ -23,6 +21,10 @@ pub struct FormData {
 // what should/how they should be captured
 // (e.g. naming each field of `form`) -
 // we can explicitly tell `tracing` to ignore them using the `skip` directive.
+// subscribe orchestrates the work to be done
+// by calling the required routines and translates
+// their outcome into the proper response
+// according to the rules and conventions of the HTTP protocol.
 #[tracing::instrument(
     // name can be used to specify
     // the message associated to the function span
@@ -39,31 +41,43 @@ pub async fn subscribe(
     form: web::Form<FormData>,
     pool: web::Data<PgPool>,
 ) -> HttpResponse {
-    let query_span = tracing::info_span!(
-        "Saving new subscriber details in the database"
-    );
+    match insert_subscriber(&pool, &form).await
+    {
+        Ok(_) => HttpResponse::Ok().finish(),
+        Err(_) => HttpResponse::InternalServerError().finish()
+    }
+}
 
-    match sqlx::query!(
+// `insert_subscriber` takes care of the
+// database logic and it has no awareness of
+// the surrounding web framework - i.e.
+// we are not passing `web::Form` or `web::Data` wrappers as input types
+#[tracing::instrument(
+name = "Saving new subscriber details in the database",
+skip(form, pool)
+)]
+pub async fn insert_subscriber(
+    pool: &PgPool,
+    form: &FormData,
+) -> Result<(), sqlx::Error> {
+    sqlx::query!(
         r#"
-        INSERT INTO subscriptions (id, email, name, subscribed_at)
-        VALUES ($1, $2, $3, $4)
-        "#,
+    INSERT INTO subscriptions (id, email, name, subscribed_at)
+    VALUES ($1, $2, $3, $4)
+            "#,
         Uuid::new_v4(),
         form.email,
         form.name,
         Utc::now()
     )
-    .execute(pool.as_ref())
-    // First we attach the instrumentation, then we `.await` it
-    .instrument(query_span)
+    .execute(pool)
     .await
-    {
-        Ok(_) => HttpResponse::Ok().finish(),
-        Err(e) => {
-            // Yes, this error log falls outside of `query_span`
-            // We'll rectify it later, pinky swear!
-            tracing::error!("Failed to execute query: {:?}", e);
-            HttpResponse::InternalServerError().finish()
-        }
-    }
+    .map_err(|e| {
+        tracing::error!("Failed to execute query: {:?}", e);
+        e
+    // Using the `?` operator to return early
+    // if the function failed, returning a sqlx::Error
+    // We will talk about error handling in depth later!
+    })?;
+    Ok(())
 }
