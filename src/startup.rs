bf0a382aae2ae65b4b92a68d6b8f7fb47497c0a8
“suchapalaver” <jlivesey@gmail.com>
2022-04-12 16:21:57 -0400

    Use `tracing-actix-web` to enrich all logs with a request_id property

--- src/startup.rs
+++ src/startup.rs
@@ -2,18 +2,20 @@
 
 use crate::routes::{health_check, subscribe};
 use actix_web::dev::Server;
-use actix_web::middleware::Logger;
+use actix_web::web::Data;
 use actix_web::{web, App, HttpServer};
 use sqlx::PgPool;
 use std::net::TcpListener;
+use tracing_actix_web::TracingLogger;
 
 pub fn run(listener: TcpListener, db_pool: PgPool) -> Result<Server, std::io::Error> {
-    // Wrap the pool using web::Data, which boils down to an Arc smart pointer
-    let db_pool = web::Data::new(db_pool);
+    // Wrap the pool using `Data`, which boils down to an Arc smart pointer
+    let db_pool = Data::new(db_pool);
     let server = HttpServer::new(move || {
         App::new()
             // Middlewares are added using the `wrap` method on `App`
-            .wrap(Logger::default())
+            // Instead of `Logger::default` (.wrap(Logger::default())
+            .wrap(TracingLogger::default())
             .route("/health_check", web::get().to(health_check))
             .route("/subscriptions", web::post().to(subscribe))
             .app_data(db_pool.clone())
