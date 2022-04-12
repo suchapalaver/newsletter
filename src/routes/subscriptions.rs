cb1ef102f3ad75b41b168e136de847317fd82aa8
“suchapalaver” <jlivesey@gmail.com>
2022-04-12 13:52:19 -0400

    Use `tracing::instrument` to wrap `subscribe` function in a span

--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -13,27 +13,35 @@ pub struct FormData {
     name: String,
 }
 
-pub async fn subscribe(form: web::Form<FormData>, pool: web::Data<PgPool>) -> HttpResponse {
-    let request_id = Uuid::new_v4();
-    // Spans, like logs, have an associated level
-    // `info_span` creates a span at the info-level
-    let request_span = tracing::info_span!(
-        "Adding a new subscriber.",
-        %request_id,
+// `#[tracing::instrument]` creates a span
+// at the beginning of the function invocation and
+// automatically attaches all arguments
+// passed to the function to the context of the span - in our case,
+// `form` and `pool`.
+// Often function arguments won’t be displayable on log records
+// (e.g. `pool`) or we’d like to specify more explicitly
+// what should/how they should be captured
+// (e.g. naming each field of `form`) -
+// we can explicitly tell `tracing` to ignore them using the `skip` directive.
+#[tracing::instrument(
+    // name can be used to specify
+    // the message associated to the function span
+    // - if omitted, it defaults to the function name.
+    name = "Adding a new subscriber",
+    skip(form, pool),
+    fields(
+        request_id = %Uuid::new_v4(),
         subscriber_email = %form.email,
-        subscriber_name = %form.name
+        subscriber_name= %form.name
+    )
+)]
+pub async fn subscribe(
+    form: web::Form<FormData>,
+    pool: web::Data<PgPool>,
+) -> HttpResponse {
+    let query_span = tracing::info_span!(
+        "Saving new subscriber details in the database"
     );
-    // info_span returns the newly created span,
-    // but we have to explicitly step into it using
-    // the .enter() method to activate it.
-    let _request_span_guard = request_span.enter();
-    // Enters this span, returning a guard that
-    // will exit the span when dropped.
-
-    // We do not call `.enter` on query_span!
-    // `.instrument` takes care of it at the right moments
-    // in the query future lifetime
-    let query_span = tracing::info_span!("Saving new subscriber details in the database");
 
     match sqlx::query!(
         r#"
