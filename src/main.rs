9d66cc76d08a9c1c91c20570bfd1c99bec270d9a
“suchapalaver” <jlivesey@gmail.com>
2022-04-13 13:00:27 -0400

    Run `rustfmt` on src/main.rs

--- src/main.rs
+++ src/main.rs
@@ -13,10 +13,9 @@ async fn main() -> std::io::Result<()> {
 
     let configuration = get_configuration().expect("Failed to read configuration.");
     // No longer async, given that we don't actually try to connect!
-    let connection_pool = PgPool::connect_lazy(
-        &configuration.database.connection_string().expose_secret()
-        )
-        .expect("Failed to create Postgres connection pool.");
+    let connection_pool =
+        PgPool::connect_lazy(&configuration.database.connection_string().expose_secret())
+            .expect("Failed to create Postgres connection pool.");
     let address = format!("127.0.0.1:{}", configuration.application_port);
     let listener = TcpListener::bind(address)?;
     run(listener, connection_pool)?.await?;
