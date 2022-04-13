93a1d3da248f15674dbb686a197e6c3b731662f1
“suchapalaver” <jlivesey@gmail.com>
2022-04-13 18:54:42 -0400

    Shorten default timeout to acquire connection from pool in `sqlx`

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,7 @@ use newsletter::configuration::get_configuration;
 use newsletter::startup::run;
 use newsletter::telemetry::{get_subscriber, init_subscriber};
 use secrecy::ExposeSecret;
-use sqlx::postgres::PgPool;
+use sqlx::postgres::PgPoolOptions;
 use std::net::TcpListener;
 
 #[tokio::main]
@@ -13,11 +13,10 @@ async fn main() -> std::io::Result<()> {
 
     let configuration = get_configuration().expect("Failed to read configuration.");
     // No longer async, given that we don't actually try to connect!
-    let connection_pool =
-        PgPool::connect_lazy(
-            &configuration.database.connection_string().expose_secret()
-            )
-            .expect("Failed to create Postgres connection pool.");
+    let connection_pool = PgPoolOptions::new()
+        .connect_timeout(std::time::Duration::from_secs(2))
+        .connect_lazy(&configuration.database.connection_string().expose_secret())
+        .expect("Failed to create Postgres connection pool.");
     let address = format!(
         "{}:{}",
         configuration.application.host, configuration.application.port
