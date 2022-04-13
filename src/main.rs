dec7cc384bfbccabc94ee45307db3208e227ff59
“suchapalaver” <jlivesey@gmail.com>
2022-04-13 16:19:24 -0400

    Fix missing error handling of `connection_pool` in src/main.rs

--- src/main.rs
+++ src/main.rs
@@ -14,7 +14,9 @@ async fn main() -> std::io::Result<()> {
     let configuration = get_configuration().expect("Failed to read configuration.");
     // No longer async, given that we don't actually try to connect!
     let connection_pool =
-        PgPool::connect_lazy(&configuration.database.connection_string().expose_secret())
+        PgPool::connect_lazy(
+            &configuration.database.connection_string().expose_secret()
+            )
             .expect("Failed to create Postgres connection pool.");
     let address = format!(
         "{}:{}",
