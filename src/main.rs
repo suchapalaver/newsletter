d27162740be86b650fa51de987a8938fb717b5bb
“suchapalaver” <jlivesey@gmail.com>
2022-04-28 12:17:11 -0400

    Extract logic from `main` to `startup`

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,9 @@
 //! src/main.rs
 use newsletter::{
     configuration::get_configuration,
-    email_client::EmailClient,
-    startup::run,
+    startup::build,
     telemetry::{get_subscriber, init_subscriber},
 };
-use sqlx::postgres::PgPoolOptions;
-use std::net::TcpListener;
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
@@ -14,32 +11,8 @@ async fn main() -> std::io::Result<()> {
     init_subscriber(subscriber);
 
     let configuration = get_configuration().expect("Failed to read configuration.");
-    // No longer async, given that we don't actually try to connect!
-    let connection_pool = PgPoolOptions::new()
-        .connect_timeout(std::time::Duration::from_secs(2))
-        // `connect_lazy_with` instead of `connect_lazy`
-        .connect_lazy_with(configuration.database.with_db());
+    let server = build(configuration).await?;
 
-    // Build an `EmailClient` using `configuration`
-    let sender_email = configuration
-        .email_client
-        .sender()
-        .expect("Invalid sender email address.");
-    let timeout = configuration.email_client.timeout();
-    let email_client = EmailClient::new(
-        configuration.email_client.base_url,
-        sender_email,
-        configuration.email_client.authorization_token,
-        // Pass new argument from configuration
-        timeout
-    );
-
-    let address = format!(
-        "{}:{}",
-        configuration.application.host, configuration.application.port
-    );
-    let listener = TcpListener::bind(address)?;
-    // New argument for `run`, `email_client`
-    run(listener, connection_pool, email_client)?.await?;
+    server.await?;
     Ok(())
 }
--- src/startup.rs
+++ src/startup.rs
@@ -1,4 +1,5 @@
 //! src/startup.rs
+use crate::configuration::Settings;
 use crate::{
     email_client::EmailClient,
     routes::{health_check, subscribe},
@@ -6,9 +7,37 @@ use crate::{
 use actix_web::dev::Server;
 use actix_web::web::Data;
 use actix_web::{web, App, HttpServer};
-use sqlx::PgPool;
+use sqlx::{PgPool, postgres::PgPoolOptions};
 use std::net::TcpListener;
 use tracing_actix_web::TracingLogger;
+
+pub async fn build(configuration: Settings) -> Result<Server, std::io::Error> {
+    let connection_pool = PgPoolOptions::new()
+        .connect_timeout(std::time::Duration::from_secs(2))
+        .connect_lazy_with(configuration.database.with_db());
+
+    // Build an `EmailClient` using `configuration`
+    let sender_email = configuration
+        .email_client
+        .sender()
+        .expect("Invalid sender email address.");
+
+    let timeout = configuration.email_client.timeout();
+    let email_client = EmailClient::new(
+        configuration.email_client.base_url,
+        sender_email,
+        configuration.email_client.authorization_token,
+        // Pass new argument from configuration
+        timeout
+    );
+
+    let address = format!(
+        "{}:{}",
+        configuration.application.host, configuration.application.port
+    );
+    let listener = TcpListener::bind(address)?;
+    run(listener, connection_pool, email_client)
+}
 
 pub fn run(
     listener: TcpListener,
