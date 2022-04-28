b584737e0d8f26e682245977d03244fe5df1daa6
“suchapalaver” <jlivesey@gmail.com>
2022-04-28 17:05:53 -0400

    Change insertion query to set `status` to `confirmed`

--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -71,12 +71,9 @@ pub async fn insert_subscriber(
     new_subscriber: &NewSubscriber,
 ) -> Result<(), sqlx::Error> {
     sqlx::query!(
-        r#"
-    INSERT INTO subscriptions (id, email, name, subscribed_at)
-    VALUES ($1, $2, $3, $4)
-        "#,
+        r#"INSERT INTO subscriptions (id, email, name, subscribed_at, status)
+        VALUES ($1, $2, $3, $4, 'confirmed')"#,
         Uuid::new_v4(),
-        // Using `as_ref` now!
         new_subscriber.email.as_ref(),
         new_subscriber.name.as_ref(),
         Utc::now()
