728188369cfa6f5822c72417edfa78c187a30bcc
“suchapalaver” <jlivesey@gmail.com>
2022-05-03 16:14:31 -0400

    Tweak `subscribe` request handler to satisfy new test

--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -63,14 +63,23 @@ pub async fn subscribe(
     if insert_subscriber(&pool, &new_subscriber).await.is_err() {
         return HttpResponse::InternalServerError().finish();
     }
+
+    let confirmation_link = "https://my-api.com/subscriptions/confirm";
     // Send a (useless) email to the new subscriber.
     // We are ignoring email delivery errors for now.
     if email_client
         .send_email(
             new_subscriber.email,
             "Welcome!",
-            "Welcome to our newsletter!",
-            "Welcome to our newsletter!",
+            &format!(
+                "Welcome to our newsletter!<br />\
+                Click <a href=\"{}\">here</a> to confirm your subscription.",
+                confirmation_link
+            ),
+            &format!(
+                "Welcome to our newsletter!\nVisit {} to confirm your subscription.",
+                confirmation_link
+            ),
         )
         .await
         .is_err()
