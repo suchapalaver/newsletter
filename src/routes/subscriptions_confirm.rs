69b2dbb43d190f769f2aa56027bf493956f30338
“suchapalaver” <jlivesey@gmail.com>
2022-05-03 18:50:23 -0400

    Add `subscription_token` query parameter to `confirm`

--- src/routes/subscriptions_confirm.rs
+++ src/routes/subscriptions_confirm.rs
@@ -1,10 +1,16 @@
 //! src/routes/subscriptions_confirm.rs
 
-use actix_web::HttpResponse;
+use actix_web::{HttpResponse, web};
+
+#[derive(serde::Deserialize)]
+pub struct Parameters {
+    subscription_token: String
+}
 
 #[tracing::instrument(
     name = "Confirm a pending subscriber",
+    skip(_parameters)
 )]
-pub async fn confirm() -> HttpResponse {
+pub async fn confirm(_parameters: web::Query<Parameters>) -> HttpResponse {
     HttpResponse::Ok().finish()
 }
